e2d6afee39dee0890e90d124cf18b34eb80adee5
vignesh <12137@ramco.com>
2020-08-08 20:58:13 +0530

    gitcommans

---
+++ git_commands.sql
@@ -0,0 +1,47 @@
+---GIT
+
+-----to download the git 
+https://git-scm.com/download/win
+
+--git bash
+
+git clear 	  					-- to clear screen
+
+git config 						--list  - which show basic config
+
+git config --global user.name  	 "Vignesh"
+
+goit config --global user.email  "121387@ramco.com"
+
+
+---GIt first project
+
+---git bash
+
+cd  "Dirctoty path"
+
+git init  -- to create repostory 
+
+git status  -- to check whether project created or not 
+
+
+touch hello.py --to creat empty file in git path
+
+git status --- to check the status of the file and project
+
+git add hello.py -- to adding to stage area (git place)
+
+git commit -m "my first commit using git"
+
+
+git log --- to check history of git project
+
+git diff --- to check change in the file
+
+git ls-files  ---- list the files in project
+
+git rm <filename1> <filename1> --- remove the file from reposirty
+
+git rm  -r <directory> - sub dirctory remove the path
+
+
